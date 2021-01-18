c826fe83babde161a1bb7b49067585fa1e5b2c15
Bruna Mello <brunacrespomello@gmail.com>
2021-01-18 10:21:33 +0000

    Salvando Usuário no Banco de Dados

--- src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
+++ src/main/java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -1,14 +1,25 @@
 package br.com.caelum.vraptor.controller;
 
+import javax.inject.Inject;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.model.Usuario;
 
 @Path("dashboard")
 @Controller
 public class DashboardController {
+	
+	//injetar usuario pelo vraptor
+	@Inject EntityManager em; 
+	
+	//injetar usuario para o dashboard 
+	@Inject Result result;
 	
 	@Get("")
 	public void dashboard() {
@@ -17,10 +28,13 @@ public class DashboardController {
 	
 	@Post("salvausuario")
 	public void salvaUsuario(Usuario usuario) {
-		//falta fazer
-		//validar usuario 
-		//salvar no banco 
-		//direcionar para o dashboard
+		
+		//salvar usuario no banco
+		em.persist(usuario);
+		
+		//apos salvar o usuario direcionar para o dashboard
+		result.redirectTo(DashboardController.class).dashboard();
+				
 	}
 
 }
--- src/main/java/br/com/caelum/vraptor/model/Usuario.java
+++ src/main/java/br/com/caelum/vraptor/model/Usuario.java
@@ -1,9 +1,33 @@
 package br.com.caelum.vraptor.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //gerar valor do id automatico na tabela 
+	
+	private Integer id;
+	
+	@NotEmpty // so vale para string, para outras usar @notnull
+	@Size(min=4, max=20) //configurando tamanho char
 	private String nome;
+	
+	@NotEmpty
+	@Email // validando email 
 	private String email;
+	
+	@NotEmpty
+	@Size(min=6, max=20)
 	private String senha;
 	
 	
@@ -29,8 +53,11 @@ public class Usuario {
 	public void setSobrenome(String sobrenome) {
 		this.nome += " " +sobrenome;
 	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	
-	
-	
-
 }
